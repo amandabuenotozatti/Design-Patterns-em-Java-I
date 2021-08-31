b9d6ec3371b1660b5c3cc3c31002a498941de35c
Amanda Tozatti <amandabuenotozatti@gmail.com>
2021-08-30 21:58:30 -0300

    Aplicando o pattern State

--- loja/src/Orcamento/Orcamento.java
+++ loja/src/Orcamento/Orcamento.java
@@ -1,15 +1,37 @@
 package Orcamento;
 
+import Orcamento.Situacao.Aprovado;
+import Orcamento.Situacao.EmAnalise;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens){
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
+    }
+
+    public void aplicarDescontoExtra(){
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(orcamento);
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar(){
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar(){
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar(){
+        this.situacao.finalizar(this);
     }
 
     public BigDecimal getValor(){
@@ -19,4 +41,13 @@ public class Orcamento {
     public int getQuantidadeItens(){
         return quantidadeItens;
     }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
+    }
+
 }
